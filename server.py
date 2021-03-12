f9cae30ef784d94e7e6f138bdc85b08561fddaae
Logan Frederick <loganfrederick@hotmail.com>
2021-03-12 07:52:12 -0500

    expand debug env var to reload for static file changes

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import jsonify
 from flask import render_template
+import os
 
 import apis
 
 app = Flask(__name__)
 
+load_dotenv('config.env')
 
 @app.route('/api/bitcoin')
 def bitcoin():
@@ -30,3 +32,17 @@ def trivia():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if os.getenv('DEBUG') == '1':
+    extra_dirs = ['static',]
+    extra_files = extra_dirs[:]
+    for extra_dir in extra_dirs:
+        for dirname, dirs, files in os.walk(extra_dir):
+            for filename in files:
+                filename = os.path.join(dirname, filename)
+                if os.path.isfile(filename):
+                    extra_files.append(filename)
+    app.run(extra_files=extra_files)
+else:
+    app.run()
